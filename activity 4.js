7c1362c6bfd2dc4a64322cc17b236eaa53f6b50c
JuuJep <122791849+JuuJep@users.noreply.github.com>
2023-01-17 14:00:10 +0200

    Add files via upload

---
+++ activity 4.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+       <body>
+       <div id="hello-world"></div>
+              <script>
+              document.getElementByld("hello-world").innerHTML = "Hello World!";
+              </script>
+       </body>
+</html>
